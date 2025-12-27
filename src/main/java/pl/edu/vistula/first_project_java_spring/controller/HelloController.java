bcaf1a9b77fe0005a5c9b097a45d733d7e4cc095
Ayberfin <aerdem1@stu.vistula.edu.pl>
2025-12-27 20:07:44 +0100

    Add README and screenshots for Task 1

--- src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
@@ -16,7 +16,6 @@ public class HelloController {
     public String greeting(@RequestParam(name="name", required = false, defaultValue = "World") String name, Model model){
         model.addAttribute("name", name);
         return "greeting";
-
-
     }
+    //http://localhost:8080/greeting?name=Vistula
 }
